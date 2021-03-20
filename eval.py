336a8d3dca1d1d4c1cb66a81c12dce80286ecc31
minerva <minerva.suvanto@gmail.com>
2021-03-20 14:27:25 +0100

    progress

--- eval.py
+++ eval.py
@@ -3,38 +3,47 @@ import torch
 import train
 import pandas as pd
 
-# def load_data(file):
-#     # TODO not sure what to do here
-#     text_rows = []
-#     with open(file, "r") as f:
-#         text_rows = [line for line in f]
-#     return text_rows
-
 
 def get_instances(test_data):
+    # TODO should use training data for feature lengths..?
     # data = load_data(args.test_data) 
     data, vocab = train.a(test_data) # Load the test data
+    # tensor([[[0., 0., 0.,  ..., 0., 0., 0.],
+    #      [0., 0., 0.,  ..., 0., 1., 0.],
+    #      [0., 0., 0.,  ..., 0., 0., 0.],
+    #      ...,
+    #      [0., 0., 0.,  ..., 0., 0., 0.],
+    #      [0., 0., 0.,  ..., 0., 0., 0.],
+    #      [0., 0., 0.,  ..., 0., 0., 0.]]])
+    # y = []
+    # X = []
+    # vowels = sorted(['y', 'é', 'ö', 'a', 'i', 'å', 'u', 'ä', 'e', 'o'])
+    # print(len(p))
+    
+    # z = np.zeros(113)
+    # z[p.index(x)] = 1
 
-# tensor([[[0., 0., 0.,  ..., 0., 0., 0.],
-#          [0., 0., 0.,  ..., 0., 0., 0.],
-#          [0., 0., 0.,  ..., 0., 0., 0.],
-#          ...,
-#          [0., 0., 0.,  ..., 0., 0., 0.],
-#          [0., 0., 0.,  ..., 0., 0., 0.],
-#          [0., 0., 0.,  ..., 0., 0., 0.]]])
-    # torch.Tensor()
+    # for v in range(len(data) - 4):
+    #     if data[v+2] not in vowels:
+    #         continue
+        
+    #     h2 = vowels.index(u[v+2])
+    #     y.append(h2)
+    #     r = np.concatenate([g(x, p) for x in [data[v], data[v+1], data[v+3], data[v+4]]])
+    #     X.append(r)
 
-    instances = train.b(data, vocab) # Create evaluation instances compatible with the training instances.  
+    # np.array(X), np.array(y)
+
+    X, y = train.b(data, vocab) # Create evaluation instances compatible with the training instances.  
     # (A simplifying assumption for the purposes of the assignment: assuming that the neighbouring 
     # vowels are known as though the Fairy hadn't stolen them.)
-    df = pd.DataFrame(instances[0])
-    # df['class'] = instances[1]
-    # featuredf = df.drop('class', axis=1)
-    # classdf = df['class']
+    df = pd.DataFrame(X)
+    df['class'] = y
+    featuredf = df.drop('class', axis=1)
+    classdf = df['class']
     tensor = torch.Tensor(df.to_numpy())
-    # tensor = torch.Tensor(featuredf.to_numpy()), torch.LongTensor(classdf.to_numpy())
-    # print(tensor.unsqueeze(1))
-    return tensor.unsqueeze(1)
+    tensor = torch.Tensor(featuredf.to_numpy()), torch.LongTensor(classdf.to_numpy())
+    return tensor
     
 
 if __name__ == "__main__":
@@ -47,10 +56,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     model = torch.load(args.model) # Load a model produced by train.py
+    model.eval()
 
-    instances = get_instances(args.test_data)
-    # predictions = model(instances[0]) # Use the model to predict instances
-    # print(predictions)
+    testing_instances, actual_classes = get_instances(args.test_data)
+    outputs = model(testing_instances.unsqueeze(0)) # Use the model to predict instances
+    predictions = pd.Series(outputs.squeeze(0).argmax(dim=1).numpy())
+
+    # print(train.vowels)
     # Write the text with the predicted (as opposed to the real) vowels back into an output file.
     # f = open("predictions.txt", "w")
     # f.write(predictions)
